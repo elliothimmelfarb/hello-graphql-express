cf5bc9fc38717a60507b7a6feaa9dc2a12cefda5
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-06-21 21:04:04 -0600

    express server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const root = { hello: () => 'Hello world!' };
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(5000, () => console.log('Serving on localhost:5000/'));
