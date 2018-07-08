3f6fb4e7fa09f73047a5806e9ed47bec3beeaa51
elliothimmelfarb <elliothimmelfarb@gmail.com>
2018-07-07 19:25:05 -0600

    querying binance api

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
+const axios = require('axios')
 
 const schema = buildSchema(`
   type Query {
     hello: String
+    binance: OrderBook
+  }
+
+  type OrderBook {
+    bids: [Entry]
+    asks: [Entry]
+  }
+
+  type Entry {
+    quantity: String
+    price: String
   }
 `);
 
-const root = { hello: () => 'Hello world!' };
+const root = {
+  hello: () => 'Hello world!',
+  binance: () => axios.get(`https://api.binance.com/api/v1/depth?symbol=ETHBTC`)
+    .then(res => {
+      res.data.bids = res.data.bids.map(e => ({
+        quantity: e[1],
+        price: e[0],
+      }))
+      res.data.asks = res.data.asks.map(e => ({
+        quantity: e[1],
+        price: e[0],
+      }))
+      return res.data
+    })
+
+};
 
 const app = express();
 
